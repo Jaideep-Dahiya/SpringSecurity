17c8b2859f43862e49230f2c007b297588c2e4c4
Jaideep Dahiya <jaideepdahiya@Jaideeps-MacBook-Pro.local>
2023-08-28 20:14:09 +0530

    01_login security

---
+++ src/main/java/com/jd/springsecurtiybasic/SpringsecurtiybasicApplication.java
@@ -0,0 +1,13 @@
+package com.jd.springsecurtiybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurtiybasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurtiybasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jd/springsecurtiybasic/SpringsecurtiybasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jd.springsecurtiybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurtiybasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
