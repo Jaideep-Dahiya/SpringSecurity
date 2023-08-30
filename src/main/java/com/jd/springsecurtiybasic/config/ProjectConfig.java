2e0793ed2664184fed94a8e621bbfa6f944a1b8d
Jaideep Dahiya <jaideepdahiya@Jaideeps-MacBook-Pro.local>
2023-08-30 18:48:48 +0530

    custom securityfilterchain bean

---
+++ src/main/java/com/jd/springsecurtiybasic/config/ProjectConfig.java
@@ -0,0 +1,20 @@
+package com.jd.springsecurtiybasic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests( requests -> requests
+                        .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                        .requestMatchers("/notices","/contact","/welcome").permitAll())
+                        .formLogin(Customizer.withDefaults())
+                        .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
